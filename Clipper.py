db7c69bc93056bf645eac1a681427235351c7fe0
L-H-A <47578325+L-H-A@users.noreply.github.com>
2021-01-31 12:01:53 -0800

    finalise

---
+++ Clipper.py
@@ -0,0 +1,134 @@
+from Utilities import * 
+import os 
+
+''' This module prompts user to enter json file name for twitch stream chat
+    The json file must be formatted as twitch chat format
+    User will enter parameters, or choose to use default values'''
+
+if __name__ == '__main__': 
+    while(True):
+        file_path = input('Enter json file path (with .json, exit to exit): ') 
+        
+        if type(file_path) != str: 
+            print('invalid value entered, try again') 
+            continue 
+        elif file_path == 'exit': 
+            exit(0) 
+        elif not os.path.isfile(file_path): 
+            print('file path entered invalid, try again') 
+            continue 
+        else: 
+            break 
+        
+        
+    chat_window = 10 
+    min_clip_len = 10
+    chat_speed_threshold = 100 
+    while(True): 
+        answer = input('Do you want ot use default values? (1 for yes, -1 for no, 0 for more info):') 
+        try: 
+            answer = int(answer) 
+        except: 
+            print('invalid value entered, try again')
+            continue 
+        if answer not in {0,1,-1}: 
+            print('invalid value entered, try again')
+            continue 
+        elif answer == 0: 
+            print(f'value for chat window is {chat_window}')
+            print(f'the value for min_clip_len is {min_clip_len}')
+            print(f'the value for threshold is {chat_speed_threshold}')
+            continue
+        elif answer == 1: 
+            break 
+        elif answer == -1:
+            while(True): 
+                chat_window_answer = input('Enter a chat window as an int (0 for more info): ') 
+                try: 
+                    chat_window_answer = int(chat_window_answer) 
+                except: 
+                    print('invalid value entered, try again')
+                    continue 
+                if type(chat_window_answer) != int or chat_window_answer < 0: 
+                    print('invalid value entered, try again') 
+                    continue 
+                elif chat_window_answer == 0: 
+                    print('chat window means how many chat messages we measure chat speed') 
+                    print('for example, window os 1 is that we measure chat speed at every chat message') 
+                    print('window of 10 means we average chat speed for every 10 chat: ')
+                    print('so we have a speed for chat[0:10], and another for chat[1:11]')
+                    print(f'the default value is {chat_window}')
+                    continue
+                elif chat_window_answer > 0: 
+                    chat_window = chat_window_answer
+                    break 
+                
+                while(True): 
+                    clip_len_answer = input('Enter minimum clip length as an int (0 for more info): ') 
+                    try: 
+                        clip_len_answer = int(clip_len_answer) 
+                    except: 
+                        print('invalid value entered, try again')
+                        continue 
+                    if clip_len_answer < 0: 
+                        print('invalid value, try again') 
+                        continue 
+                    elif clip_len_answer == 0: 
+                        print('clip length means how many chat minimum do we need in a clip') 
+                        print('for example, if minimum is 10')
+                        print('if a clip has 9 or less chat messages in it, we discard it') 
+                        print('the default value is 10') 
+                        continue
+                    elif clip_len_answer > 0: 
+                        min_clip_len = clip_len_answer 
+                        break 
+                        
+                while(True): 
+                    speed_thr_ans = input('Enter chat speed threshold in percent (0 for more info): ') 
+                    try: 
+                        speed_thr_ans = int(speed_thr_ans) 
+                    except: 
+                        print('invalid value entered, try again')
+                        continue 
+                    if speed_thr_ans < 0: 
+                        print('value invalid, try again') 
+                        continue 
+                    elif speed_thr_ans == 0: 
+                        print('speed threshold means at what chat speed do we start clipping') 
+                        print('each vod has its average chat speed, we take a percentage based on that') 
+                        print('for example, if the answer is 120 and a vod has average chat speed of 5/second') 
+                        print('then clipping will start when chat speed exceed 6/second') 
+                        print('the default is 100 percent')
+                        continue 
+                    elif speed_thr_ans > 0: 
+                        chat_speed_threshold = speed_thr_ans 
+                        break 
+            break 
+        
+            
+    try: 
+        with open(file_path, encoding='utf-8') as f: 
+            data = json.load(f) 
+            
+        comments = data['comments'] 
+        speed, chats, x = Twitch_Comment_to_data(comments, chat_window) 
+        clips = Clip_from_Chat(speed, chats, x, min_clip_len) 
+    except: 
+        print('file format is not correct, exiting')
+        exit(0)
+                
+                
+    while(True): 
+        clip_file_name = input('How do you want to name this pickle file? (without .pkl): ') 
+        if type(clip_file_name) != str: 
+            print('value invalid, try again') 
+            continue 
+        else: 
+            break 
+        
+    if (not os.path.exists('clip_data')) or (not os.path.isdir('clip_data')): 
+        os.makedirs('clip_data')
+    with open(os.path.join('clip_data', clip_file_name + '.pkl'), 'wb') as f: 
+        pickle.dump(clips, f)
+    exit(0)
+    
---
+++ Utilities.py
@@ -0,0 +1,58 @@
+import numpy as np 
+import os 
+import json 
+import pickle 
+
+def Twitch_Comment_to_data(comments:dict, chat_window=10): 
+    ''' takes a twitch comment formatted dict, outputs tuple of three numpy arrays
+        first is speed, second is chat string data, third is time points of those chats'''
+    offsets = list()
+    chats = list() 
+    for i in comments: 
+        offsets.append(i['content_offset_seconds']) 
+        chats.append(i['message']['body']) 
+        
+    chats = chats[chat_window::]
+    chats = np.char.array(chats, unicode=True) 
+    offsets = np.array(offsets, dtype=float)
+    speed = offsets[chat_window::] - offsets[:-chat_window:]
+    x = np.array(offsets[chat_window::],dtype=float) 
+    speed = speed + 1.0/chat_window
+    speed = 1/speed
+    return (speed,chats,x)
+
+
+class clip_it(): 
+    def __init__(self, start:float):
+        self.start_time = start
+        self.chats = list() 
+        self.end_time = -1
+
+    def __len__(self): 
+        return len(self.chats)  
+
+
+
+def Clip_from_Chat(speed, chats, time_points, min_len=5): 
+    ''' takes chat speed and string numpy array, turn into a list of clip class objects 
+        it will not clip if clip chat is less then 5 in length'''
+    assert len(speed) == len(chats)
+    assert len(chats) == len(time_points)
+    N = len(speed) 
+    i = 0
+    avg_speed = speed.mean()
+    clips = list()
+    while(i<N):
+        if speed[i] >= avg_speed: 
+            clip = clip_it(time_points[i])
+            while(i<N and speed[i] >= avg_speed): 
+                clip.chats.append(chats[i]) 
+                i+=1 
+            if len(clip) >= min_len:
+                clip.end_time = time_points[i]
+                clips.append(clip)
+            
+        i+=1
+        
+        
+    return clips 
