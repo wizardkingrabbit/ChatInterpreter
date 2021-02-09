0bc2ef11a54de0519904050f307eee85c169c8b8
L-H-A <47578325+L-H-A@users.noreply.github.com>
2021-02-08 17:30:36 -0800

    monday changes

--- Tokenizer_kit.py
+++ Tokenizer_kit.py
@@ -1,18 +1,55 @@
 # this module defines tokenizing tools for us to use in leaner modules. 
-# typically tokanizing take a long string
+# typically tokanizing take a long string, returns a collection of tokens
 # thus any processing to get the long string should be done outside the tokenizing functions
 
 import os 
 import numpy as np 
+import nltk 
+from nltk.corpus import stopwords 
+from collections import defaultdict
 
-# ================================= this part is for methods to make long string from collections =======================
+nltk_stop_words = set(stopwords.words('english'))
+
+# =============================== small methods/tools ==================================================================== 
+
+def Default_word_filter(word:str) -> bool: 
+    ''' by default, every word is counted'''
+    return True 
+
+def Default_word_modifier(word:str) -> str: 
+    ''' by default, words are not modified'''
+    return word 
+
+def BOW_to_str(bow:dict, freq_order=True, top_k=-1, indent='') -> str: 
+    ''' This method turns a bow into user-friendly string for printing 
+        printing order can be by frequency, or by a-b-c if by freq is false 
+        the number of word to print out can be specified by top k 
+        default, all words are printed out, with the specified indent'''
+    to_return = '' 
+    if freq_order:
+        items = sorted(bow.items(), key=lambda x: x[1], reverse=True) 
+    else: 
+        items = sorted(bow.items(), key=lambda x: x[0]) 
+        
+    for word, freq in items: 
+        to_return += indent 
+        to_return += word 
+        to_return += ' -> ' 
+        to_return += str(freq) 
+        to_return += os.linesep 
+        top_k -= 1 
+        if top_k == 0: 
+            break 
+    return to_return
+    
+
+# ================================= methods to make long string from collections =======================
 
 
-def concatenate_str_list(str_list:list, random_order:False, splitter=os.linesep) -> str: 
+def Concatenate_str_list(str_list:list, random_order=False, splitter=os.linesep) -> str: 
     ''' This function take a list of string, concatenate them into a long string 
         random order means items are concatenated in random order 
         splitter is what to put in between items in the long string''' 
-        
     if not random_order: 
         to_return = splitter.join(str_list) 
         return to_return
@@ -28,4 +65,53 @@ def concatenate_str_list(str_list:list, random_order:False, splitter=os.linesep)
     
     
 
-# =============================== this part is for processing long strings ==============================================
+# =============================== this part is for methods long strings into lists ==============================================
+
+def Simple_tokenizer(long_string:str, remove_stop_words=True, case_sensitive=False) -> list: 
+    ''' This module tokenize a long string, tokenize by white space, retaining marks such as question marks
+        parameter options are whether to remove stop_words (from nltk module), and whether it is case sensitive 
+        return value is a list of tokens made'''
+    if not case_sensitive: 
+        long_string = long_string.lower() 
+    tokens = nltk.tokenize.word_tokenize(long_string) 
+    if remove_stop_words: 
+        to_return = [w for w in tokens if not(w in nltk_stop_words)] 
+    else: 
+        to_return = list(tokens) 
+    return to_return 
+    
+
+
+# ================================ methods for processing list of tokens into BOW (sets, dicts, etc) ============================
+
+def List_to_bow(token_list:list, filter_func=Default_word_filter, modifier_func=Default_word_modifier, n_gram=1, connector=''): 
+    ''' takes a list of tokens, make them into a bag of words based on some conditions 
+        n grams will be conted, default is 1, they will be connected with connector string specified
+        first, the raw word has to pass a filter, by default, all are passed 
+        second, the raw word is modified, by default, they remain the same 
+        ''' 
+    to_return = defaultdict(int) 
+    ngram_window = [] 
+    for token in token_list: 
+        assert type(token) == str, 'passed token not string' 
+        if not filter_func(token): 
+            continue 
+        ngram_window.append(token) 
+        if len(ngram_window) < n_gram: 
+            continue
+        if len(ngram_window) > n_gram: 
+            ngram_window.pop(0) 
+        for i in range(len(ngram_window)): 
+            word = connector.join(ngram_window[:i+1]) 
+            to_return[modifier_func(word)] += 1 
+            
+
+    while(len(ngram_window) > 0): 
+        ngram_window.pop(0)
+        for i in range(len(ngram_window)): 
+            word = connector.join(ngram_window[:i+1]) 
+            to_return[modifier_func(word)] += 1 
+
+    return to_return 
+
+    
--- foo.py
+++ foo.py
@@ -2,12 +2,28 @@ from Utilities import *
 import os 
 import pickle 
 from Clip import *
+from Tokenizer_kit import *
 
 
-with open('clip_data/test.pkl', 'rb') as f: 
+with open('clip_data/TeosGame[2].pkl', 'rb') as f: 
     data = pickle.load(f) 
     
-    
-sample = data[0] 
-print(len(sample.time_stamps)) 
-print(len(sample.chats)) 
+
+print(len(data)) 
+print(type(data))
+sample = data[20] 
+# print(sample) 
+print(type(sample)) 
+
+long_string = Concatenate_str_list(sample.chats) 
+print(long_line) 
+print('this is the long string: ') 
+print(long_string) 
+
+token_list = Simple_tokenizer(long_string) 
+
+bow = List_to_bow(token_list, n_gram=1, connector='-') 
+
+print(long_line) 
+print('this is bow: ') 
+print(BOW_to_str(bow, indent=' ', top_k=10)) 
