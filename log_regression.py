78dfbec2328b891a73eb1d4ebc4b1a801233182d
wizardkingrabbit <jingtil2@uci.edu>
2021-02-15 15:35:05 +0800

    replace the tokenizer with the one Hank wrote

--- log_regression.py
+++ log_regression.py
@@ -23,50 +23,6 @@ warnings.filterwarnings(action='ignore')
 
 # nltk.download('stopwords') is needed
 # the following functions are from HW1, with some modification.
-
-def parts_of_speech(s):
-	tokens = word_tokenize(s)
-	tokens_and_tags = nltk.pos_tag(tokens)
-	n = 0
-	temp = set()
-	for item in tokens_and_tags:
-		if (not item[1] in temp):
-			temp.add(item[1])
-			n += 1
-	print(" the total number of tokens is " + str(n))
-	tags = [ item[1] for item in tokens_and_tags ]
-	tag_counts = nltk.FreqDist(tags)
-	sorted_tag_counts = tag_counts.most_common()
-	for item in sorted_tag_counts:
-		tag_percent = 100 * item[1]/n
-		p = '{0:.2f}'.format(tag_percent)
-		print('Tag:',item[0],'\t   Percentage of tokens = ', p )
-	return(tokens_and_tags)
-
-def create_bow_from_reviews(clips, special_stops = None):
-	text = []
-	Y = []
-	lengths = []
-	print('\nExtracting tokens from each review.....(can be slow for a large number of reviews)......')   
-	for clip in clips:
-		review = Tokenizer_kit.Concatenate_str_list(clip.chats)
-		stars = clip.get_label_binary()
-		if (stars == -1):
-			stars = 1
-		if (stars == 1):
-			text.append(review)
-			Y.append('1')
-		if (stars == 0):
-			text.append(review)   
-			Y.append('0')
-	if (special_stops == None):
-		vectorizer = CountVectorizer(ngram_range = (1, 2), stop_words = 'english', min_df = 0.01)
-	else:
-		vectorizer = CountVectorizer(ngram_range = (1, 2), stop_words = special_stops, min_df = 0.01)
-	print("show vectorizer: ", vectorizer)
-	X = vectorizer.fit_transform(text)
-	print('Data shape: ', X.shape)
-	return X, Y, vectorizer
     
 def logistic_classification(X, Y, test_fraction):
 	if (test_fraction == 0):
@@ -144,35 +100,74 @@ def main():
 
 	if (run_mode == "all"):
 		test_frac = Utilities.prompt_for_float("Test fraction is ", 0, 1)
-		clip_list = []
+		text = []
+		Y = []
 		for filename in os.listdir("labeled_clip_data"):
 			the_file = open("labeled_clip_data/" + filename, 'rb')
 			the_pkl = pickle.load(the_file)
 			for clip in the_pkl:
-				clip_list.append(clip)
-		xx, yy, vect = create_bow_from_reviews(clip_list, special_stop_word)
-		classifier = logistic_classification(xx, yy, test_frac)
+				text.append(Tokenizer_kit.Concatenate_str_list(clip.chats))
+				if (clip.get_label_binary() == 0):
+					Y.append(0)
+				else:
+					Y.append(1)
+
+		concat_str = Tokenizer_kit.Concatenate_str_list(text)
+		token_list = Tokenizer_kit.Simple_tokenizer(concat_str)
+		the_bow = Tokenizer_kit.List_to_bow(token_list)
+
+		if (special_stop_word == None):
+			vect = CountVectorizer(ngram_range = (1, 2), stop_words = 'english', min_df = 0.01)
+		else:
+			vect = CountVectorizer(ngram_range = (1, 2), stop_words = special_stop_word, min_df = 0.01)
+		X = vect.fit_transform(text)
+		classifier = logistic_classification(X, Y, test_frac)
 
 	if (run_mode == "single"):
-		clip_list = []
+		text = []
+		Y = []
 		train_file_path = Utilities.prompt_for_file("which labeled file you want to use?")
 		the_file = open(train_file_path, 'rb')
 		the_pkl = pickle.load(the_file)
 		for clip in the_pkl:
-			clip_list.append(clip)
-		xx, yy, vect = create_bow_from_reviews(clip_list, special_stop_word)
-		classifier = logistic_classification(xx, yy, 0)
+			text.append(Tokenizer_kit.Concatenate_str_list(clip.chats))
+			if (clip.get_label_binary() == 0):
+				Y.append(0)
+			else:
+				Y.append(1)
+		concat_str = Tokenizer_kit.Concatenate_str_list(text)
+		token_list = Tokenizer_kit.Simple_tokenizer(concat_str)
+		the_bow = Tokenizer_kit.List_to_bow(token_list)
+		if (special_stop_word == None):
+			vect = CountVectorizer(ngram_range = (1, 2), stop_words = 'english', min_df = 0.01)
+		else:
+			vect = CountVectorizer(ngram_range = (1, 2), stop_words = special_stop_word, min_df = 0.01)
+		X = vect.fit_transform(text)
+		classifier = logistic_classification(X, Y, 0)
+
 		while (input("enter y to test the classifier against another labeled file, enter other to quit") == "y"):
 			# this feature is not finished!
 			# how can I use a classifier built according to one word set on another word set
-			clip_list = []
+			text = []
+			Y = []
 			test_file_path = Utilities.prompt_for_file("which labeled file you want to use?")
 			the_file = open(test_file_path, 'rb')
 			the_pkl = pickle.load(the_file)
 			for clip in the_pkl:
-				clip_list.append(clip)
-			xx, yy, _ = create_bow_from_reviews(clip_list, special_stop_word)
-			test_accuracy = classifier.score(xx, yy)
+				text.append(Tokenizer_kit.Concatenate_str_list(clip.chats))
+				if (clip.get_label_binary() == 0):
+					Y.append(0)
+				else:
+					Y.append(1)
+			concat_str = Tokenizer_kit.Concatenate_str_list(text)
+			token_list = Tokenizer_kit.Simple_tokenizer(concat_str)
+			the_bow = Tokenizer_kit.List_to_bow(token_list)
+			if (special_stop_word == None):
+				vect = CountVectorizer(ngram_range = (1, 2), stop_words = 'english', min_df = 0.01)
+			else:
+				vect = CountVectorizer(ngram_range = (1, 2), stop_words = special_stop_word, min_df = 0.01)
+			X = vect.fit_transform(text)
+			test_accuracy = classifier.score(X, Y)
 			print("the test accuracy is " + str(test_accuracy))
 
 	if (input("enter y to look at top 5 significant terms, enter other to quit") == "y"):
