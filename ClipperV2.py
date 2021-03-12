2d190aa0b09cf64195b388b48deb884cc1bfa6b8
L-H-A <47578325+L-H-A@users.noreply.github.com>
2021-03-12 14:34:36 -0800

    final changes before making demo

--- ClipperV2.py
+++ ClipperV2.py
@@ -24,7 +24,8 @@ rnn = torch.load('rnn.pt')
 with open(vod_chrono,'rb') as f: 
     data = pickle.load(f) 
 
-interval = data[1][0]-data[0][0]
+interval = data[1][0]-data[0][0] 
+vod_duration = data[-1][0]-data[0][0]
 vod_intervals = np.zeros(len(data), dtype=np.float32) 
 kv = Load_wv(kv_path)
 print(f"processing data") 
@@ -49,36 +50,11 @@ for i in range(len(chats)):
         vod_intervals[i]=1
         Print_progress(i,len(chats),message='marked 1') 
     i+=1
-
-# chat_vec = np.expand_dims(chat_vec,axis=1) 
-# chat_tensor = torch.from_numpy(chat_vec) 
-
-# hidden = rnn.initHidden() 
-# for i in range(chat_tensor.size()[0]): 
-#         output, hidden = rnn(chat_tensor[i], hidden) 
-        
-#         topv, topi = output.topk(output.size()[1], dim=1, largest=True)
-#         softmax = nn.Softmax(dim=1)
-#         vod_intervals[i] = int(topi[0][0]) 
-#         Print_progress(i,chat_tensor.size()[0])
-        
-# # for i in range(len(data)): 
-# #     chat = data[i][1] 
-# #     if chat=='': 
-# #         vod_intervals[i]=0 
-# #         continue
-# #     chat_list = chat.split(os.linesep) 
-# #     vec = Chat_to_2d_vec(chat_list=chat_list, kv=kv, window=1, overlap=0) 
-# #     vec = np.vstack([vec for i in range(10)])
-# #     # print(vec.shape)
-# #     vec = np.expand_dims(vec,axis=1)
-# #     vod_intervals[i] = Predict(rnn,(vec,0)) 
-# #     Print_progress(i,len(data)) 
     
 mask = np.ones(2*interval,dtype=np.float32) 
 mask = mask/np.sum(mask) 
 assert np.abs(np.sum(mask)-1)<0.1
-print(f"prediction sum is [{np.sum(vod_intervals)}]/[{len(chats)}]")
+# print(f"prediction sum is [{np.sum(vod_intervals)}]/[{len(chats)}]")
 
 vod_intervals = scipy.ndimage.correlate(vod_intervals,mask,mode='constant',cval=0) 
 vod_intervals = (vod_intervals>0.1) 
@@ -130,6 +106,7 @@ for i,interval in enumerate(results):
     total_time+=(interval[1]-interval[0])
 
     
-print(f"total time to edit is [{time_to_str(total_time)}]")
+print(f"total time to edit is [{time_to_str(total_time)}]") 
+print(f"original vod duration is [{time_to_str(vod_duration)}]")
      
 
